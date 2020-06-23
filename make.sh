22c506c8d124e55da3228e2b130cbcaf1b99097e
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-06-23 09:17:24 +0200

    complete

--- make.sh
+++ make.sh
@@ -1,21 +1,20 @@
-
 function create-secrets(){
-    kubectl create secret generic db-user-pass  --from-file=./private/password.txt
-    kubectl create secret generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
-    kubectl create secret generic odatests-secret-key  --from-file=./private/secret-key.txt
-    kubectl create secret generic minio-key  --from-file=./private/minio-key.txt
-    kubectl create secret generic jena-password  --from-file=./private/jena-password.txt
-    kubectl create secret generic logstash-entrypoint  --from-file=./private/logstash-entrypoint.txt
+    kubectl -n staging-1-3 create secret generic dda-interface-token  --from-file=./private/token.txt
+#    kubectl create secret generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
+#    kubectl create secret generic odatests-secret-key  --from-file=./private/secret-key.txt
+#    kubectl create secret generic minio-key  --from-file=./private/minio-key.txt
+#    kubectl create secret generic jena-password  --from-file=./private/jena-password.txt
+#    kubectl create secret generic logstash-entrypoint  --from-file=./private/logstash-entrypoint.txt
 }
 
 function install() {
     set -x
-    helm --name ${NAMESPACE:?}-dqueue --namespace ${NAMESPACE:?} install . --set image.tag="$(cd dqueue; git describe --always)"
+    helm --name ${NAMESPACE:?}-dda-interface --namespace ${NAMESPACE:?} install . --set image.tag="$(cat dda-interface/image-tag)"
 }
 
 function upgrade() {
     set -x
-    helm upgrade ${NAMESPACE:?}-dqueue . --set image.tag="$(cd dqueue; git describe --always)"
+    helm upgrade ${NAMESPACE:?}-dda-interface . --set image.tag="$(cat dda-interface/image-tag)"
 }
 
 $@
