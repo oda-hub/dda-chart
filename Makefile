eb771a48b3937ba8cdfb128834b47a3ae7ab5586
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-03-23 12:29:58 +0100

    various adjustments for pvc etc

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 deploy:
 	bash make.sh upgrade || bash make.sh install
+
+create-secrets:
+	bash make.sh create-secrets
--- make.sh
+++ make.sh
@@ -1,8 +1,12 @@
-export NAMESPACE=${NAMESPACE:-staging-1-3}
+export ODA_NAMESPACE=${ODA_NAMESPACE:-staging-1-3}
 
 function create-secrets(){
-     echo
-    kubectl -n staging-1-3 create secret generic dda-interface-token  --from-file=./private/token.txt
+     set -x
+
+     echo -e "\033[33mcreating secrets for dda in $ODA_NAMESPACE\033[0m"
+
+     kubectl -n $ODA_NAMESPACE delete secret dda-interface-token || echo ok
+     kubectl -n $ODA_NAMESPACE create secret generic dda-interface-token  --from-file=./private/token.txt
 
 #    kubectl create secret generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
 #    kubectl create secret generic odatests-secret-key  --from-file=./private/secret-key.txt
@@ -12,13 +16,12 @@ function create-secrets(){
 }
 
 function install() {
-    set -x
-    helm install dda --namespace ${NAMESPACE:?}  . --set image.tag="$(cat dda/image-tag)"
+    upgrade
 }
 
 function upgrade() {
     set -x
-    helm upgrade -n ${NAMESPACE:?} dda . \
+    helm upgrade --install -n ${ODA_NAMESPACE:?} dda . \
         --set image.tag="$(cat dda/image-tag)" \
         --set securityContext.runAsUser=5182 \
         --set securityContext.runAsGroup=4700
