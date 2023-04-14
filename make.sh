d243f9db4ea1bc4f39f67579b95aa6bd9a0e6100
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2023-04-14 15:23:30 +0200

    cleaner integrated

--- make.sh
+++ make.sh
@@ -24,4 +24,14 @@ function upgrade() {
         #--set securityContext.runAsGroup=4915
 }
 
+function clean-loop() {
+    set -x
+    while true; do
+        kubectl exec -n $ODA_NAMESPACE -it deployment/oda-dda-interface -- find /scratch -name poke -cmin +10 -type f -exec rm -rfv '{}' \;
+        kubectl exec -n $ODA_NAMESPACE -it deployment/oda-dda-interface -- find /scratch -size +10M -cmin +10 -type f -exec rm -fv '{}' \;
+        kubectl exec -n $ODA_NAMESPACE -it deployment/oda-dda-interface -- find /scratch -size +1k -cmin +600 -type f -exec rm -fv '{}' \;
+        sleep 600
+    done
+}
+
 $@
